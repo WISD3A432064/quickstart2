623ab332fe8107753c97290c99ed60ebb9eb2b48
nini <poikl15397@gmail.com>
2017-10-18 18:20:42 +0800

    '建立使用者認證'

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+
+    //使用者認證
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
